169d184ec45300d1762834abe7c549d741c6c1fa
Arpit Mohapatra <mohapatra.arpit2003@cet.edu.in>
2023-08-14 02:33:23 +0530

    updated

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const cors=require('cors')
+const morgan =require('morgan')
 const connectDB = require("./config/db")
+const userRoutes = require("./routes/userRoutes")
+const postRoutes = require("./routes/postRoutes")
+const { notFound, errorHandler } = require("./middleware/errorMiddleware")
 
 dotenv.config()
 
 const app = express()
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
 
 connectDB()
     .then(async connection =>{
@@ -13,8 +22,14 @@ connectDB()
     .catch(e => console.error(e))
 
 app.get("/", (req, res) => {
-    res.send("Resonse to API")
+    res.send("API running successfully")
 })
+
+app.use("/api/user", userRoutes)
+app.use("/api/post", postRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
